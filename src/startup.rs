48d723bec55e88ed2593b25c8b657ce68a1c78c0
Nigel <nigel.nderi@gmail.com>
2024-03-30 00:01:04 -0400

    updating github workflow settings

--- src/startup.rs
+++ src/startup.rs
@@ -3,12 +3,17 @@ use std::net::TcpListener;
 
 use crate::routes;
 
-
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(routes::health_check::health_check))
-            .route("/subscriptions", web::post().to(routes::subscriptions::subscribe))
+            .route(
+                "/health_check",
+                web::get().to(routes::health_check::health_check),
+            )
+            .route(
+                "/subscriptions",
+                web::post().to(routes::subscriptions::subscribe),
+            )
     })
     .listen(listener)?
     .run();
